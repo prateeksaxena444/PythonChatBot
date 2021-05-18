e6fe2b5a5559f539880ea31d281ddfd2d5d38d2e
Prateek Saxena <psaxena@yotta.com>
2021-05-18 12:03:44 +0530

    latest

--- chatbot-master/main.py
+++ chatbot-master/main.py
@@ -1,16 +1,15 @@
 import ast
-
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
 from tkinter import *
 import pyttsx3 as pp
 import speech_recognition as s
 import threading
 import tkinter.messagebox
 import webbrowser
-from PIL import ImageTk, Image
+from tkinter import simpledialog
+import json
 from functools import partial
-from requests.structures import CaseInsensitiveDict
+import ctypes
 
 engine = pp.init()
 
@@ -19,52 +18,44 @@ print(voices)
 
 engine.setProperty('voice', voices[0].id)
 
-
-
 # pyttsx3
-bot = ChatBot("FiinFormer")
 
+bot = ChatBot("ASK YOTTA")
 
 def openNewWindow():
-    tkinter.messagebox.showinfo("About us","FiinFormer can be used to resolve your queries related to Financial Markets.Also it has features like,'listen to Bot' ,'FAQ's and 'Link to accesss Investopidia'.\n\n It is developed by a team involving 5 members: 'Aditya Thakkar, Sahim Reza, Rohit Salvi, Prateek Saxena and Vaidehi Degwekar'")
+    tkinter.messagebox.showinfo("About us","ASK YOTTA is Created by IT-Engineering and Development Team - Yotta Infrastructure Solutions")
 
 main = Tk()
 
 main.geometry("800x650")
-main.title("FiinFormer")
+main.title("ASK YOTTA")
 
 
-btnabs = Button(main, text="About us", font=("Verdana", 8), bg="light blue", command=openNewWindow, bd="5", relief="groove")
+btnabs = Button(main, text="About us", font=("Verdana", 8), bg="white", command=openNewWindow, bd="5", relief="groove")
 btnabs.pack(side=TOP , anchor='w')
 
-img = PhotoImage(file="bot2.png")
+img = PhotoImage(file="iCON.png")
 
-photoL = Label(main, image=img, bd="6", relief="groove")
+photoL = Label(main, image=img,bd="6", relief="groove")
 
 photoL.pack(pady=5)
 
 
-
 def getresponse(query):
-    file = open("data.txt", "r")
+    file = open("data1.json", "r")
     contents = file.read()
     d = ast.literal_eval(contents)
     file.close()
-    print(d)
     new_d=lower_dict(d)
     query_lowercase=query.lower()
     value=''
-    print(new_d.keys())
-    list_ofValues=[]
-    list_ofValues=list(new_d.keys())
-
-    for key in new_d:
-         print(type(key))
+    if value == '':
+     list_ofValues=[]
+     list_ofValues=list(new_d.keys())
+     for key in new_d:
          if (query_lowercase in key):
-                print('yes')
                 value=new_d.__getitem__(key)
                 break
-
     return value
 
 
@@ -76,21 +67,20 @@ def speak():
     engine.say(answer_to_speak)
     engine.runAndWait()
 
-
-# take query : it takes audio as input from user and convert it to string..
-
+# take query : it takes audio as input from user and convert it to string
 def takeQuery():
     sr = s.Recognizer()
     sr.pause_threshold = 1
     print("your bot is listening try to speak")
     with s.Microphone() as m:
         try:
-            audio = sr.listen(m)
+            sr.adjust_for_ambient_noise(m, duration=0.2)
+            audio = sr.listen(m, timeout=3, phrase_time_limit=3)
             query = sr.recognize_google(audio, language='eng-in')
             print(query)
             textF.delete(0, END)
             textF.insert(0, query)
-            ask_from_bot()
+            ask_from_bot(query)
         except Exception as e:
             print(e)
             print("not recognized")
@@ -102,16 +92,13 @@ def show_hand_cursor(b):
 def hide_hand_cursor(b):
     msgs.config(cursor="")
 
-
 def callbackLink(substring,tag):
     webbrowser.open(substring)
 
 def handleHttp(answer_from_bot):
    if 'http' in answer_from_bot:
         startindex = answer_from_bot.index('http')
-        print(startindex)
         substring = answer_from_bot[startindex:]
-        print(substring)
         endindex = answer_from_bot.index(substring)
         msgs.insert(END, "\n\nbot : " + answer_from_bot[:endindex], "answer")
         msgs.tag_config("a", foreground="blue", underline=1)
@@ -126,15 +113,12 @@ def handleHttp(answer_from_bot):
 
 answer_to_speak = "You have to ask something first";
 
-
-def ask_from_bot():
-    query = textF.get()
+def ask_from_bot(str):
+    query = str
     msgs.insert(END, "\n\nyou : " + query.upper())
     answer_from_bot = getresponse(query)
-    print(type(answer_from_bot))
     f = open('stopword.txt', 'r')
     contents_stop = f.read()
-    print(contents_stop)
     f.close()
     if query.lower() in contents_stop:
         msgs.tag_config("noanswer", foreground="red")
@@ -142,8 +126,30 @@ def ask_from_bot():
         msgs.insert(END, "\n\nbot : " + str(answer_from_bot), "noanswer")
     else:
         if (answer_from_bot != ''):
-            #msgs.tag_config("answer", foreground="blue")
-            #msgs.insert(END, "\n\nbot : " + str(answer_from_bot), "answer")
+            msgs.tag_config("answer", foreground="blue")
+            handleHttp(answer_from_bot)
+        else:
+            msgs.tag_config("noanswer", foreground="red")
+            answer_from_bot = "Sorry,I did not get you, please try to ask your question in other way"
+            msgs.insert(END, "\n\nbot : " + str(answer_from_bot), "noanswer")
+    global answer_to_speak
+    answer_to_speak = answer_from_bot
+    textF.delete(0, END)
+    msgs.yview(END)
+
+def ask_from_bot():
+    query = textF.get()
+    msgs.insert(END, "\n\nyou : " + query.upper())
+    answer_from_bot = getresponse(query)
+    f = open('stopword.txt', 'r')
+    contents_stop = f.read()
+    f.close()
+    if query.lower() in contents_stop:
+        msgs.tag_config("noanswer", foreground="red")
+        answer_from_bot="Sorry,I did not get you, please try to ask your question in other way"
+        msgs.insert(END, "\n\nbot : " + str(answer_from_bot), "noanswer")
+    else:
+        if (answer_from_bot != ''):
             msgs.tag_config("answer", foreground="blue")
             handleHttp(answer_from_bot)
         else:
@@ -156,10 +162,9 @@ def ask_from_bot():
     msgs.yview(END)
 
 def ask_from_faq():
-    query = "What is Equity?"
+    query = "Darwin box link"
     answer_from_bot = getresponse(query.lower())
     msgs.insert(END, "\n\nyou : " + query.upper())
-    print(type(answer_from_bot))
     msgs.tag_config("answer", foreground="blue")
     handleHttp(answer_from_bot)
     global answer_to_speak
@@ -169,10 +174,9 @@ def ask_from_faq():
 
 
 def ask_from_btn2():
-    query = "What are bonds?"
+    query = "Monitoring portal link"
     answer_from_bot = getresponse(query.lower())
     msgs.insert(END, "\n\nyou : " + query.upper())
-    print(type(answer_from_bot))
     msgs.tag_config("answer", foreground="blue")
     handleHttp(answer_from_bot)
     global answer_to_speak
@@ -182,10 +186,9 @@ def ask_from_btn2():
 
 
 def ask_from_btn3():
-    query = "What are primary markets?"
+    query = "Tell me about prateek saxena"
     answer_from_bot = getresponse(query.lower())
     msgs.insert(END, "\n\nyou : " + query.upper())
-    print(type(answer_from_bot))
     msgs.tag_config("answer", foreground="blue")
     handleHttp(answer_from_bot)
     global answer_to_speak
@@ -195,10 +198,9 @@ def ask_from_btn3():
 
 
 def ask_from_btn4():
-    query = "What is IPO?"
+    query = "Prateek's Team"
     answer_from_bot = getresponse(query.lower())
     msgs.insert(END, "\n\nyou : " + query.upper())
-    print(type(answer_from_bot))
     msgs.tag_config("answer", foreground="blue")
     handleHttp(answer_from_bot)
     global answer_to_speak
@@ -206,49 +208,33 @@ def ask_from_btn4():
     textF.delete(0, END)
     msgs.yview(END)
 
-def  ask_from_btn5():
-    query = "Mutual fund definition"
-    answer_from_bot = getresponse(query.lower())
-    msgs.insert(END, "\n\nyou : " + query.upper())
-    print(type(answer_from_bot))
-    msgs.tag_config("answer", foreground="blue")
-    handleHttp(answer_from_bot)
-    global answer_to_speak
-    answer_to_speak = answer_from_bot
-    textF.delete(0, END)
-    msgs.yview(END)
-
-def  ask_from_btn6():
-    query = "What are Debt Instruments?"
-    answer_from_bot = getresponse(query.lower())
-    msgs.insert(END, "\n\nyou : " + query.upper())
-    print(type(answer_from_bot))
-    msgs.tag_config("answer", foreground="blue")
-    handleHttp(answer_from_bot)
-    global answer_to_speak
-    answer_to_speak = answer_from_bot
-    textF.delete(0, END)
-    msgs.yview(END)
-
-def ask_from_btn7():
-        query = "What is dematerialization?"
-        answer_from_bot = getresponse(query.lower())
-        msgs.insert(END, "\n\nyou : " + query.upper())
-        print(type(answer_from_bot))
-        msgs.tag_config("answer", foreground="blue")
-        handleHttp(answer_from_bot)
-        global answer_to_speak
-        answer_to_speak = answer_from_bot
-        textF.delete(0, END)
-        msgs.yview(END)
-
+def add_question(filename='data1.json'):
+    with open('data1.json') as json_file:
+        data = json.load(json_file)
+        question = simpledialog.askstring(title="Question Box",
+                                               prompt="Enter Question :  ")
+        answer = simpledialog.askstring(title="Answer Box",
+                                              prompt="Enter Answer:  ")
+        temp = data
+        y = {question : answer}
+        temp.update(y)
+    with open(filename,'w') as f:
+        json.dump(data, f, indent=4)
+        ctypes.windll.user32.MessageBoxW(0, "Your question is added Successfully", "SUCCESS", 1)
 
 
 frame = Frame(main)
 
 photo = PhotoImage(file = "speakup.png")
-btn5 = Button(main, text="Speak up please ?", font=("Verdana", 10), image=photo, command=speak)
+btn5 = Button(main, text="Answer in audio format ?", font=("Verdana", 10), image=photo, command=speak)
 btn5.pack(side=TOP)
+
+voiceQuerybtn= Button(main, text="Voice call to BOT ?", font=("Verdana", 10), command=takeQuery)
+voiceQuerybtn.pack(side=TOP)
+
+
+add_questionBtn = Button(main, text="Add a Question", font=("Verdana", 10),command=add_question)
+add_questionBtn.pack(side=TOP)
 
 sc = Scrollbar(frame)
 msgs = Text(frame, width=170, height=18, yscrollcommand=sc.set, xscrollcommand=sc.set, wrap=WORD, background='white', bd="6",relief="ridge")
@@ -277,41 +263,28 @@ textF.pack(fill=X, pady=10)
 btn = Button(main, text="Ask the Bot", fg="dark blue", font=("Verdana", 12), bd= "4", command=ask_from_bot)
 btn.pack()
 
-w = Label(main, text="FAQ's :  ", font=("Verdana", 8), bg="dark blue", foreground="white", bd="5", relief="groove")
+w = Label(main, text="FAQ's :  ", font=("Verdana", 8),bg="black", foreground="white", bd="5", relief="groove")
 w.pack(side=LEFT)
 
-btn1 = Button(main, text="What is Equity ?", font=("Verdana", 8), bg="light blue", command=ask_from_faq, bd="5")
+btn1 = Button(main, text="Darwin box link", font=("Verdana", 8), bg="white", command=ask_from_faq, bd="5")
 btn1.pack(side=LEFT)
 
-btn2 = Button(main, text="What are bonds?", font=("Verdana", 8), bg="light blue", command=ask_from_btn2, bd="5")
+btn2 = Button(main, text="Monitoring portal link", font=("Verdana", 8), bg="white", command=ask_from_btn2, bd="5")
 btn2.pack(side=LEFT)
 
-btn3 = Button(main, text="What are primary markets?", font=("Verdana", 8), bg="light blue", command=ask_from_btn3, bd="5")
+btn3 = Button(main, text="Tell me about prateek saxena", font=("Verdana", 8), bg="white", command=ask_from_btn3, bd="5")
 btn3.pack(side=LEFT)
 
-btn4 = Button(main, text="What is IPO?", font=("Verdana", 8), bg="light blue", command=ask_from_btn4, bd="5")
+btn4 = Button(main, text="Prateek's Team", font=("Verdana", 8), bg="white", command=ask_from_btn4, bd="5")
 btn4.pack(side=LEFT)
 
-btn5 = Button(main, text="Mutual fund definition", font=("Verdana", 8), bg="light blue", command=ask_from_btn5, bd="5")
-btn5.pack(side=LEFT)
-
-btn6 = Button(main, text="What are Debt Instruments?", font=("Verdana", 8), bg="light blue", command=ask_from_btn6, bd="5")
-btn6.pack(side=LEFT)
-
-btn7 = Button(main, text="What is dematerialization?", font=("Verdana", 8), bg="light blue", command=ask_from_btn7, bd="5")
-btn7.pack(side=LEFT)
-
 def callback(event):
-    webbrowser.open_new(r"http://www.investopedia.com/")
+    webbrowser.open_new("https://care.yotta.com/")
 
 
-link = Label(main, text="Link to Investopedia", fg="dark blue", cursor="hand2", bd="5", relief="groove")
+link = Label(main, text="Link to Yotta Cloud Portal", fg="dark blue", cursor="hand2", bd="5", relief="groove")
 link.pack(side=RIGHT,anchor='e')
 link.bind("<Button-1>", callback)
-
-
-
-
 
 # creating a function
 def enter_function(event):
@@ -325,7 +298,7 @@ main.bind('<Return>', enter_function)
 
 def repeatL():
     while True:
-        takeQuery()
+     takeQuery()
 
 
 t = threading.Thread(target=repeatL)
